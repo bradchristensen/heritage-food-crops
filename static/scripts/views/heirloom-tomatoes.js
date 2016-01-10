6c41c458fe5858b829c2a1d79b58930eda97d390
Brad Christensen <brad@christensen.co.nz>
2016-01-10 17:20:23 +1300

    Add new interactive table of tomato varieties (WIP)

--- static/scripts/views/heirloom-tomatoes.js
+++ static/scripts/views/heirloom-tomatoes.js
@@ -12,7 +12,89 @@ import _ from 'lodash';
 import OutboundLink from 'components/outboundLink';
 import { Link } from 'react-router';
 
+var tomatoTable = [
+    ['Amish Yellowish Orange Oxheart', 4.44, 0, 8.21, 0, 0, 1],
+    ['Alice Brewer (orange)', 4.40, 0.02, 3.82, 0, 0, 35],
+    ['Alice Brewer (orange)', 4.27, 0.02, 1.55, 0, 0, 58],
+    ['Amana Orange', 4.30, 0.07, 5.04, 0, 0.01, 22],
+    ['Amish Orange Sherbert Heirloom', 4.04, 0, 5.36, 0, 0, 18],
+    ['Aunt Gerties Gold', 4.20, 0.06, 5.73, 0, 0, 14],
+    ['Barnes Mountain Orange', 4.30, 0, 6.54, 0, 0, 8],
+    ['Big Orange', 4.39, 0.03, 6.14, 0, 0, 10],
+    ['Big Yellow', 4.17, 0.04, 4.06, 0, 0.03, 33],
+    ['Brown\'s Yellow Giant', 4.29, 0, 3.57, 0, 0, 39],
+    ['Bursztyn', 4.31, 0.03, 3.34, 0, 0, 43],
+    ['Chuck\'s Golden', 4.48, 0, 5.47, 0, 0, 16],
+    ['Djena Lees Golden Girl', 4.47, 0.04, 5.23, 0, 0.04, 20],
+    ['Earl of Edgecomb', 4.04, 0.11, 2.52, 0, 0, 51],
+    ['Elbe', 4.26, 0.03, 4.31, 0, 0, 28],
+    ['Elfie (orange)', 4.23, 0.06, 4.94, 0, 0.01, 23],
+    ['Elfie (orange)', 4.20, 0.09, 3.46, 0, 0, 40],
+    ['Faribo Goldheart', 4.26, 0.03, 4.90, 0, 0, 24],
+    ['Gary Ibsen\'s Gold', 4.35, 0.03, 4.00, 0, 0, 34],
+    ['Gary\'s Golden Bear', 4.01, 0.08, 1.64, 0, 0.01, 56],
+    ['Golden Grape', 4.18, 0.06, 2.50, 0.42, 0.06, 52],
+    ['Golden Green', 4.24, 0.05, 7.12, 0, 0, 4],
+    ['Hawaiian Pineapple', 4.28, 0.03, 6.13, 0, 0, 12],
+    ['Homer Fike\'s Yellow Oxheart', 4.19, 0.05, 1.74, 0, 0.01, 54],
+    ['Hurma Ukrainian', 4.36, 0.03, 5.76, 0, 0, 13],
+    ['Ilse\'s Orange Latvian', 4.56, 0, 6.73, 0, 0, 7],
+    ['King\'s Gold (Koanga)', 4.24, 0.04, 3.02, 0, 0, 45],
+    ['Large Yellow Amish', 4.23, 0.04, 4.28, 0, 0, 29],
+    ['Moonglow*', 4.43, 0, 5.38, 0, 0, 17],
+    ['Mountain Gold', 4.40, 0.02, 3.60, 0, 0, 38],
+    ['Old Wyandotte', 4.25, 0.02, 1.65, 0, 0, 55],
+    ['Orange Beefsteak', 4.16, 0.02, 4.56, 0, 0, 26],
+    ['Olga\'s Round Golden Chicken', 4.29, 0.05, 7.27, 0, 0, 3],
+    ['Orange Bourgoin', 4.37, 0, 2.62, 0, 0, 49],
+    ['Orange Crimea', 4.27, 0.04, 8.04, 0, 0, 2],
+    ['Orange Fleshed Purple Smudge', 4.31, 0.03, 6.99, 0, 0.04, 5],
+    ['Orange Latvian', 4.31, 0.02, 0.93, 0, 0, 62],
+    ['Orange Plum', 4.27, 0.03, 1.84, 0, 0, 53],
+    ['Orange Roma', 4.32, 0.02, 6.92, 0, 0.08, 6],
+    ['Orange Strawberry', 4.29, 0.03, 4.14, 0, 0, 32],
+    ['Orange Valencia', 4.16, 0.08, 4.60, 0, 0, 25],
+    ['Pegs Round Orange', 4.32, 0.02, 1.08, 0, 0, 61],
+    ['Persimmon', 4.00, 0.02, 5.24, 0, 0, 19],
+    ['Roughwood Golden Plum', 4.34, 0.14, 3.82, 0, 0, 36],
+    ['Russian Persimmon', 4.28, 0.05, 3.10, 0, 0, 44],
+    ['Persimmon', 4.19, 0, 3.82, 0, 0, 37],
+    ['Sakharniy Zeltiy', 4.29, 0, 3.42, 0, 0, 41],
+    ['Russian Persimmon', 4.14, 0.05, 2.93, 0.52, 0.01, 46],
+    ['Sibirische Orange', 4.30, 0.05, 4.56, 0.44, 0.11, 27],
+    ['Small Lap', 4.07, 0.12, 2.87, 0, 0, 47],
+    ['Small Sweet Orange', 4.38, 0.12, 1.63, 0.66, 0.12, 57],
+    ['Summer Cider Apricot', 4.30, 0, 6.15, 0, 0, 9],
+    ['Tangella (cherry type)', 4.25, 0.03, 6.13, 0, 0, 11],
+    ['Tobolsk', 4.24, 0, 5.73, 0, 0.06, 15],
+    ['Turkey Champ', 4.16, 0.04, 4.26, 0, 0.02, 30],
+    ['Valencia', 4.38, 0.04, 1.33, 0, 0, 60],
+    ['West Virginia', 4.18, 0, 5.05, 0, 0, 21],
+    ['Yellow Brandywine Platfoot Strain', 4.28, 0.13, 4.15, 0, 0, 31],
+    ['Yellow Oxheart', 4.23, 0.02, 2.58, 0, 0, 50],
+    ['Yellow Russian', 4.40, 0.02, 1.48, 0, 0, 59],
+    ['Yoders Yellow German', 4.43, 0.03, 2.65, 0, 0.01, 48],
+    ['Yoder\'s Yellow German', 4.39, 0.02, 3.40, 0, 0, 42]
+].map(item => ({
+    genotype: item[0],
+    pH: item[1],
+    lutein: item[2],
+    tetraCisLycopene: item[3],
+    betaCarotene: item[4],
+    allTransLycopene: item[5],
+    ranking: item[6]
+}));
+
 export default React.createClass({
+    getInitialState () {
+        return {
+            tomatoTableSortColumn: 'genotype',
+            tomatoTableSortAscending: true,
+            tomatoTableHighlightedRow: null,
+            tomatoTable
+        };
+    },
+
     openLightbox (event) {
         if (event.button === 0) {
             var img = _.find(event.currentTarget.childNodes, node => node.tagName === 'IMG');
@@ -20,6 +102,19 @@ export default React.createClass({
             Actions.openLightbox(event.currentTarget.href, caption);
             event.preventDefault();
         }
+    },
+
+    sortTomatoTableByColumn (column) {
+        var sortAscending = this.state.tomatoTableSortColumn === column ?
+            !this.state.tomatoTableSortAscending : this.state.tomatoTableSortAscending;
+
+        var newTomatoTable = _.sortByOrder(tomatoTable, [column], [sortAscending ? 'asc' : 'desc']);
+
+        this.setState({
+            tomatoTableSortColumn: column,
+            tomatoTableSortAscending: sortAscending,
+            tomatoTable: newTomatoTable
+        });
     },
 
     render () {
@@ -165,12 +260,12 @@ export default React.createClass({
 
             <SectionSubheading>Top Varieties with Tetra-Cis-Lycopene</SectionSubheading>
 
-            <div style={{ padding: '0 17px' }}>
+            <div style={{ padding: '10px 25px' }}>
                 <table style={{ boxSizing: 'border-box', width: '100%', margin: '0' }}>
                     <thead>
                         <tr>
                             <th style={{ border: 'none' }}></th>
-                            <th colSpan='4' align='center' style={{ border: 'none' }}>Carotenoids (mg/100g FW)</th>
+                            <th colSpan='4' style={{ border: 'none', textAlign: 'center' }}>Carotenoids (mg/100g FW)</th>
                         </tr>
                         <tr>
                             <th style={{ border: 'none' }}>Cultivar Name</th>
@@ -352,6 +447,73 @@ export default React.createClass({
         <div className='box'>
             <SectionHeading>References</SectionHeading>
             <References />
+        </div>
+
+        <div className='box'>
+            <SectionHeading>Appendix: Tomato varieties confirmed to contain Tetra-cis-lycopene</SectionHeading>
+
+            <p>Tetra-cis-lycopene is a highly beneficial bioavailable form of lycopene, and exhibits a golden-orange or tangerine tomato colour. But not all tomatoes with this colour contain tetra-cis-lycopene, as this colour can also be expressed by beta-carotene. Hence it is necessary to chemically analyse each tomato variety to verify whether or not it contains tetra-cis-lycopene.</p>
+
+            <p>We provide here a list of all the varieties that we have had analysed and confirmed to have tetra-cis-lycopene {/*(and details of the original seed source)*/} to assist health conscious gardeners, healthy food advocates and researchers worldwide. Data has been obtained from the 2015 season.</p>
+
+            <div style={{ padding: '10px 25px' }}>
+                <table style={{ width: '100%', margin: '0' }}>
+                    <thead>
+                        <tr>
+                            <th style={{ border: 'none' }} />
+                            <th colSpan='4' style={{ textAlign: 'center' }}>Concentration (mg/100g FW)</th>
+                            <th colSpan='2' className='hide-mobile' style={{ border: 'none' }} />
+                        </tr>
+                        <tr>
+                            {[
+                                ['genotype', 'Genotype Name'],
+                                ['lutein', 'Lutein'],
+                                ['tetraCisLycopene', 'Tetra-cis-lycopene'],
+                                ['betaCarotene', 'Beta-carotene'],
+                                ['allTransLycopene', 'All-trans-lycopene'],
+                                ['pH', 'pH'],
+                                ['ranking', 'Ranking']
+                            ].map(column => ({ id: column[0], name: column[1] })).map((column, index) => {
+                                return <th key={'tomato-table-header-' + column.id}
+                                    style={{ textAlign: index === 0 ? 'left' : 'right' }}
+                                    className={'sortable-column-header' +
+                                        (this.state.tomatoTableSortColumn === column.id ? ' highlight' : '') +
+                                        (column.id === 'pH' || column.id === 'ranking' ? ' hide-mobile' : '')}
+                                    onClick={() => this.sortTomatoTableByColumn(column.id)}>
+                                    {column.name}
+                                </th>;
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.tomatoTable.map((row, index) => {
+                            return <tr key={'tomato-table-row-' + index}
+                                className={row.ranking === this.state.tomatoTableHighlightedRow ? 'highlight' : ''}
+                                onClick={event => {
+                                    if (this.state.tomatoTableHighlightedRow === row.ranking) {
+                                        this.setState({ tomatoTableHighlightedRow: null });
+                                    } else {
+                                        this.setState({ tomatoTableHighlightedRow: row.ranking });
+                                    }
+                                }}>
+                                <td style={{ textAlign: 'left' }}>{row.genotype}</td>
+                                <td style={{ textAlign: 'right' }}>{row.lutein || 'n.d.'}</td>
+                                <td style={{ textAlign: 'right' }}>{row.tetraCisLycopene || 'n.d.'}</td>
+                                <td style={{ textAlign: 'right' }}>{row.betaCarotene || 'n.d.'}</td>
+                                <td style={{ textAlign: 'right' }}>{row.allTransLycopene || 'n.d.'}</td>
+                                <td style={{ textAlign: 'right' }} className='hide-mobile'>{row.pH}</td>
+                                <td style={{ textAlign: 'right' }} className='hide-mobile'>{row.ranking}</td>
+                            </tr>;
+                        })}
+                    </tbody>
+                </table>
+            </div>
+
+            <h4>Notes on tetra-cis-lycopene levels</h4>
+
+            <p>* The level of tetra-cis-lycopene is very dependent upon the ripening state of the tomato, with maximum levels appearing to be at the optimum full ripeness stage for picking and immediate eating. Our 'Moonglow' tomatoes were picked early and therefore their level of 5.38 for tetra-cis-lycopene will be greater if left to ripen further.</p>
+            {/* <p>**Data for the 'Tangerine' variety was not available for this 2015 season, however based on its previous analysis we have estimated its level for the purpose of this table.</p> */ }
+            {/* TODO: Add Tangerine to the table */}
         </div>
     </div>
 </Article>
