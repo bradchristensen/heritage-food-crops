7819694558a47aadfa6322a8393f4d7defe2c71e
Brad Christensen <brad@christensen.co.nz>
2016-01-28 21:11:02 +1300

    Update Travis config to only deploy the master branch

---
+++ tasks/travis/deploy.sh
@@ -0,0 +1,6 @@
+mkdir build
+mv * build
+tar -czf package.tgz build
+export SSHPASS=$DEPLOY_PASS
+sshpass -e scp package.tgz $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_PATH/deploy.sh
