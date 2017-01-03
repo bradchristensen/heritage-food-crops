f434c711c8093e3fab3779999a6c3693c201831f
Brad Christensen <brad@christensen.co.nz>
2017-01-03 13:14:53 +1300

    Use new ordered list styles for tables of contents (fixes #3)

--- static/scripts/components/tableOfContents.js
+++ static/scripts/components/tableOfContents.js
@@ -2,23 +2,24 @@ import React, { PropTypes } from 'react';
 
 export default function TableOfContents(props, context) {
     return (
-        <ul className='table-of-contents'>
-            {context.tableOfContents.map(item =>
+        <ol className='table-of-contents'>
+            {context.tableOfContents.map(item => (
                 <li key={`contents-section-${item.id}`}>
-                    <a href={`#section-${item.id}`} data-scroll>{`${item.id} ${item.text}`}</a>
-                    {/* TODO: fix up the css for this, which expects the ul to be outside the li */}
-                    {!!item.children.length && <ul>
-                        {item.children.map(child =>
-                            <li key={`contents-section-${item.id}-${child.id}`}>
-                                <a href={`#section-${item.id}-${child.id}`} data-scroll>
-                                    {`${item.id}.${child.id} ${child.text}`}
-                                </a>
-                            </li>,
-                        )}
-                    </ul>}
-                </li>,
-            )}
-        </ul>
+                    <a href={`#section-${item.id}`} data-scroll>{item.text}</a>
+                    {!!item.children.length && (
+                        <ol>
+                            {item.children.map(child => (
+                                <li key={`contents-section-${item.id}-${child.id}`}>
+                                    <a href={`#section-${item.id}-${child.id}`} data-scroll>
+                                        {child.text}
+                                    </a>
+                                </li>
+                            ))}
+                        </ol>
+                    )}
+                </li>
+            ))}
+        </ol>
     );
 }
 
