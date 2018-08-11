f76202db6dc9cb2f47c094b4da8eb82fa0e27385
Brad Christensen <brad@christensen.co.nz>
2018-08-11 20:01:35 +1200

    Update homepage to use flexbox layouts

--- static/scripts/views/Index.jsx
+++ static/scripts/views/Index.jsx
@@ -19,98 +19,122 @@ function Index() {
 
                     <ul className="menu-large">
                         <li className="box">
-                            <Link to="/montys-surprise">
-                                <img
-                                    src="/static/images/layout/menu-large/apples.jpg"
-                                    className="hero"
-                                    alt="Monty's Surprise"
-                                />
-                                <h2>The Monty's Surprise apple</h2>
-                                <p>
-                                    Research coordinator Mark Christensen is credited with the
-                                    discovery of the 'anti-cancer' apple, Monty's Surprise.
-                                </p>
-                                <p>
-                                    The Trust has investigated the anti-cancer properties and
-                                    preventative benefits of the Monty's Surprise apple variety,
-                                    and now promotes the growing and distribution of this very
-                                    special variety.
-                                </p>
+                            <Link to="/montys-surprise" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/apples.jpg"
+                                        className="hero"
+                                        alt="Monty's Surprise"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>The Monty's Surprise apple</h2>
+                                    <p>
+                                        Research coordinator Mark Christensen is credited with the
+                                        discovery of the 'anti-cancer' apple, Monty's Surprise.
+                                    </p>
+                                    <p>
+                                        The Trust has investigated the anti-cancer properties and
+                                        preventative benefits of the Monty's Surprise apple variety,
+                                        and now promotes the growing and distribution of this very
+                                        special variety.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                         <li className="box">
-                            <Link to="/heirloom-tomatoes">
-                                <img
-                                    src="/static/images/layout/menu-large/tomatoes.jpg"
-                                    className="hero"
-                                    alt="Heirloom Tomatoes"
-                                />
-                                <h2>Heirloom Tomatoes</h2>
-                                <p>
-                                    Research into the nutritional benefits and cancer preventative
-                                    properties of tomatoes, which has led to evidence suggesting
-                                    that "Golden Orange" heirloom tomatoes that contain the more
-                                    bio-available tetra-cis-lycopene could be superior for human
-                                    health.
-                                </p>
+                            <Link to="/heirloom-tomatoes" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/tomatoes.jpg"
+                                        className="hero"
+                                        alt="Heirloom Tomatoes"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>Heirloom Tomatoes</h2>
+                                    <p>
+                                        Research into the nutritional benefits and cancer
+                                        preventative properties of tomatoes, which has led to
+                                        evidence suggesting that "Golden Orange" heirloom tomatoes
+                                        that contain the more bio-available tetra-cis-lycopene could
+                                        be superior for human health.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                         <li className="box">
-                            <Link to="/heirloom-beans">
-                                <img
-                                    src="/static/images/layout/menu-large/beans.jpg"
-                                    className="hero"
-                                    alt="Heirloom Beans"
-                                />
-                                <h2>Heirloom Beans</h2>
-                                <p>
-                                    Searching for, preserving and distributing heirloom bean
-                                    varieties.
-                                </p>
+                            <Link to="/heirloom-beans" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/beans.jpg"
+                                        className="hero"
+                                        alt="Heirloom Beans"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>Heirloom Beans</h2>
+                                    <p>
+                                        Searching for, preserving and distributing heirloom bean
+                                        varieties.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                         <li className="box">
-                            <Link to="/plums-peaches">
-                                <img
-                                    src="/static/images/layout/menu-large/plums.jpg"
-                                    className="hero"
-                                    alt="Plums and Peaches"
-                                />
-                                <h2>Plums and Peaches</h2>
-                                <p>
-                                    Research into the nutritional benefits of heritage plums and
-                                    peaches.
-                                </p>
+                            <Link to="/plums-peaches" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/plums.jpg"
+                                        className="hero"
+                                        alt="Plums and Peaches"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>Plums and Peaches</h2>
+                                    <p>
+                                        Research into the nutritional benefits of heritage plums and
+                                        peaches.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                         <li className="box">
-                            <Link to="/huntingtons-disease">
-                                <img
-                                    src="/static/images/layout/menu-large/huntingtons.jpg"
-                                    className="hero"
-                                    alt="Huntington's Disease"
-                                />
-                                <h2>Huntington's Disease</h2>
-                                <p>
-                                    Research into a natural treatment to treat the symptoms and
-                                    delay the onset of Huntington's disease.
-                                </p>
+                            <Link to="/huntingtons-disease" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/huntingtons.jpg"
+                                        className="hero"
+                                        alt="Huntington's Disease"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>Huntington's Disease</h2>
+                                    <p>
+                                        Research into a natural treatment to treat the symptoms and
+                                        delay the onset of Huntington's disease.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                         <li className="box">
-                            <Link to="/ancient-wheat">
-                                <img
-                                    src="/static/images/layout/menu-large/wheat.jpg"
-                                    className="hero"
-                                    alt="Ancient Wheat"
-                                />
-                                <h2>Ancient Wheat</h2>
-                                <p>
-                                    This research involves importing ancient wheat varieties into
-                                    New Zealand in order to measure comparable symptoms of any
-                                    gluten intolerance between bread made with these ancient
-                                    grains compared to their modern wheat counterparts.
-                                </p>
+                            <Link to="/ancient-wheat" className="col-wrapper">
+                                <div className="col-flex">
+                                    <img
+                                        src="/static/images/layout/menu-large/wheat.jpg"
+                                        className="hero"
+                                        alt="Ancient Wheat"
+                                    />
+                                </div>
+                                <div className="col-flex">
+                                    <h2>Ancient Wheat</h2>
+                                    <p>
+                                        This research involves importing ancient wheat varieties
+                                        into New Zealand in order to measure comparable symptoms
+                                        of any gluten intolerance between bread made with these
+                                        ancient grains compared to their modern wheat counterparts.
+                                    </p>
+                                </div>
                             </Link>
                         </li>
                     </ul>
