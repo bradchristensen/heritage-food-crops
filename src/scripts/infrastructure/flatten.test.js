766e7a7ca986f144d7587a2db64852350fc10749
Brad Christensen <brad@christensen.co.nz>
2019-01-01 20:49:41 +1300

    Remove broken link checker and add a test for flattening nodes
    The broken link checker will no longer work since the website is
    now only rendered client-side.

---
+++ src/scripts/infrastructure/flatten.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import flattenNodes from './flatten';
+
+describe('flatten', () => {
+    it('flattens the tree of React children', () => {
+        const innerDiv = React.createElement('div', { key: 1 });
+        const innerH1 = React.createElement('h1', { key: 2 });
+
+        const innerInnerDiv = React.createElement('div');
+        const span1 = React.createElement('span', { key: 1 }, innerInnerDiv);
+        const span2 = React.createElement('span', { key: 2 }, [
+            innerDiv,
+            innerH1
+        ]);
+
+        const testWrapper = React.createElement('div', {}, [
+            span1,
+            span2,
+        ]);
+
+        expect(flattenNodes(testWrapper.props.children)).toEqual([
+            span1,
+            innerInnerDiv,
+            span2,
+            innerDiv,
+            innerH1
+        ]);
+    });
+});
